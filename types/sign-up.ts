291cae29472288bf8723b48d07b5e40e217666ea
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-02-05 22:33:02 -0300

    feat: create `sign-up` type following its schema

---
+++ types/sign-up.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export type SignUpProps = z.infer<typeof signUpSchema>;
+
+export const signUpSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
