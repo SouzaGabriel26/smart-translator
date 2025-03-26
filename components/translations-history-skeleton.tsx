e1dbee5f59291f8eafdd5fdb9f025268cc2e4c93
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-03-25 21:23:08 -0300

    refactor: adjusts `translations-history-skeleton` component

--- components/translations-history-skeleton.tsx
+++ components/translations-history-skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from './ui/skeleton';
 
 export async function TranslationsHistorySkeleton() {
   return (
-    <div className="h-full space-y-4 pb-4 md:pb-0">
+    <div className="space-y-4 pb-4 md:pb-0 border rounded-md p-4 w-full md:h-[750px] flex flex-col">
       <h3 className="mb-3 text-xl font-medium">Translation History</h3>
 
       <form>
@@ -17,7 +17,7 @@ export async function TranslationsHistorySkeleton() {
         />
       </form>
 
-      <div className="border rounded overflow-y-auto h-96 p-4 bg-background">
+      <div className="overflow-y-auto h-full p-4 bg-background">
         <TranslationSkeletonCard />
         <TranslationSkeletonCard />
         <TranslationSkeletonCard />
