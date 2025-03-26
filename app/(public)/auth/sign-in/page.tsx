6ee43d58bd8fc3ebede81879c54d7cbee139f0e6
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-03-26 20:42:44 -0300

    feat: implemented `language context` approach in `/sign-in` page

--- app/(public)/auth/sign-in/page.tsx
+++ app/(public)/auth/sign-in/page.tsx
@@ -1,5 +1,12 @@
+import { getAppLanguageAction } from '@/app/actions';
 import { SignInForm } from '@/components/sign-in';
+import { getLanguageContext } from '@/config/app-language-context';
 
-export default function SignIn() {
-  return <SignInForm />;
+export default async function SignIn() {
+  const language = await getAppLanguageAction();
+  const appLanguageContext = getLanguageContext(language);
+
+  return (
+    <SignInForm appLanguageContext={appLanguageContext} language={language} />
+  );
 }
