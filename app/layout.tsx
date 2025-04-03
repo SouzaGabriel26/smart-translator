2d26469433078d4dd944b5dda93061795a50f167
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-04-02 22:00:17 -0300

    feat: creates `ThemeProvider` component using `next-themes`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { ChangeLanguageButton } from '@/components/change-language-button';
+import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
 import './globals.css';
 
@@ -8,12 +9,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-screen">
-      <body className="antialiased h-full bg-background">
-        {children}
-        <Toaster />
+    <html lang="en" className="h-screen" suppressHydrationWarning>
+      <body className="antialiased h-full">
+        <ThemeProvider attribute="class" disableTransitionOnChange>
+          <div className="bg-background h-full">
+            {children}
+            <Toaster />
 
-        <ChangeLanguageButton />
+            <ChangeLanguageButton />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
