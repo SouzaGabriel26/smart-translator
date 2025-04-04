f1332da095e07a4c97cc77f845cfb5b389452cd0
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-04-03 21:37:23 -0300

    feat: creates initial `/dashboard/leads` page to admin users

---
+++ app/(private)/dashboard/(admin)/leads/page.tsx
@@ -0,0 +1,25 @@
+import { checkUserAction } from '@/actions/auth/check-user';
+import { prismaClient } from '@/lib/prisma-client';
+
+export default async function Page() {
+  const user = await checkUserAction({ role: 'ADMIN' });
+
+  const leads = await prismaClient.plansWaitList.findMany();
+
+  return (
+    <main className="h-full p-4">
+      {leads.length === 0 && (
+        <div className="flex flex-col gap-2">
+          <h2>No leads found</h2>
+        </div>
+      )}
+
+      {leads.map((lead) => (
+        <div key={lead.email} className="flex flex-col gap-2">
+          <h2>{lead.email}</h2>
+          <p>{lead.createdAt.toString()}</p>
+        </div>
+      ))}
+    </main>
+  );
+}
