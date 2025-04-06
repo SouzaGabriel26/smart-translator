802402408289b4956e602bbf1840e35e39fe81d4
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-04-06 10:34:19 -0300

    refactor: adjusts `toggle-language` transition animation

--- app/(private)/dashboard/_components/toggle-language.tsx
+++ app/(private)/dashboard/_components/toggle-language.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from '@/components/ui/button';
 import type { AppLanguageContext } from '@/config/app-language-context';
-import { useToggleLanguage } from '@/hooks/use-toggle-language';
+import { useLanguage } from '@/contexts/LanguageContext';
 import { cn } from '@/lib/utils';
 import { ArrowRight, ArrowRightLeft } from 'lucide-react';
 import { useState } from 'react';
@@ -12,13 +12,13 @@ type ToggleLanguageProps = {
 };
 
 export function ToggleLanguage({ dashboardLanguage }: ToggleLanguageProps) {
-  const [toggleIconDegree, setToggleIconDegree] = useState(0);
-  const { languageFrom, languageTo, toggleLanguage } = useToggleLanguage();
+  const [isRotated, setIsRotated] = useState(false);
+  const { languageFrom, languageTo, toggleLanguage } = useLanguage();
   const { englishLabel, portugueseLabel } = dashboardLanguage.form;
 
   function handleClickButton() {
     toggleLanguage();
-    setToggleIconDegree((prev) => (prev === 0 ? 180 : 0));
+    setIsRotated((prev) => !prev);
   }
 
   return (
@@ -39,7 +39,7 @@ export function ToggleLanguage({ dashboardLanguage }: ToggleLanguageProps) {
         <ArrowRightLeft
           className={cn(
             'size-6 transition-transform ease-in-out duration-200',
-            `rotate-${toggleIconDegree}`,
+            isRotated ? 'rotate-180' : 'rotate-0',
           )}
         />
       </Button>
