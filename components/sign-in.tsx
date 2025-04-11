8a0add686946ddd7cb81b2b0a69078ba9d8c67c4
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-04-11 18:51:51 -0300

    feat: creates `PasswordInput` component to toggle password visibility

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -21,6 +21,7 @@ import { type ComponentPropsWithRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import type { z } from 'zod';
+import { PasswordInput } from './password-input';
 import {
   Form,
   FormControl,
@@ -112,8 +113,7 @@ export function SignInForm({
                         {language === 'en' ? 'Password' : 'Senha'}
                       </FormLabel>
                       <FormControl>
-                        <Input
-                          type="password"
+                        <PasswordInput
                           placeholder="********"
                           {...field}
                         />
