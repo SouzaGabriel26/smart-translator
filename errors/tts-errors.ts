cba39daee7ba746edae0871ce1bce7c732a9a467
SouzaGabriel26 <gabriel.asouza2608@hotmail.com>
2025-09-12 18:23:47 -0300

    feat: add custom error classes for TTS service handling

---
+++ errors/tts-errors.ts
@@ -0,0 +1,16 @@
+export class TTSServiceError extends Error {
+  constructor(message: string, public cause?: Error) {
+    super(message);
+    this.name = "TTSServiceError";
+  }
+}
+
+export class TTSRateLimitError extends TTSServiceError {
+  public retryAfter?: number;
+
+  constructor(retryAfter?: number) {
+    super("TTS rate limit exceeded");
+    this.name = "TTSRateLimitError";
+    this.retryAfter = retryAfter;
+  }
+}
